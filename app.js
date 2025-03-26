38b8872525692bdffe40480b149539b01776cd24
Vanessa Baldin <vanbaldin08@gmail.com>
2025-03-26 11:54:43 -0300

    cria rotas do API

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ import express from 'express';
 const app = express();
 const porta = 3000;
 
+//Raiz da API
+
 app.get('/', (req, res) =>{
-  res.send(`API utilizando Node.js, Express e MySQL`);
+ res.send(`API utilizando Node.js, Express e MySQL`);
+});
+
+//Exibindo todos os alunos
+app.get('/alunos',(req, res) =>{
+  res.send(`Exibindo todos os alunos`);
+});
+
+//Exibindo Um aluno
+app.get('/alunos/1',(req, res) =>{
+  res.send(`Exibindo dados de UM aluno`);
+});
+
+//Inserindo/Cadastrando/ um aluno
+app.post('/alunos',(req, res) =>{
+  res.send(`Exibindo todos os alunos`);
+});
+
+//Atualizando aluno
+app.patch('alunos/1', (req, res) =>{
+    res.send(`Atulizando dados do aluno`);
+});
+
+//Excluindo aluno
+app.delete('alunos/1', (req,res) =>{
+  res.send(`Aluno excluido com sucesso!`);
 });
 
 app.listen(porta, () =>{
