73e1839272120a881e9a4b7162477ad5bb8d6d54
Vanessa Baldin <vanbaldin08@gmail.com>
2025-03-28 11:33:06 -0300

    cria arquivo aluno.js e configura funções

---
+++ src/aluno.js
@@ -0,0 +1,31 @@
+import conexao from './banco.js';
+
+/*Funções para o CRUD */
+// Função para exibir todos os alunos do banco
+function ler(res){
+
+  //Seleciona todos os registro e exibi em ordem nome
+  const sql = "SELECT * FROM alunos ORDER BY nome";
+
+  //Executando a query a partir da conexão
+  conexao.query(sql, (erro, resultados) => {
+
+    if(resultado === 0){
+        res.status(204).end();
+        return; //forçar a interrupção do código
+    }
+      //verificação básica de erro
+    if(erro){
+        //se erro, exibir status 400 e informar qual foi o erro
+        res.status(400).json(erro.code);
+    } else { 
+        //se der certo apresenta o status 200 e exibe o resultado(no formato json)
+        res.status(200).json(resultados);
+    }
+
+
+  });
+
+}
+
+export { ler };
